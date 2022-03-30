465c14427c61a8353b22b65aac143a730d029e0f
pulkit-sigmoid <pulkitg@sigmoidanalytics.com>
2022-03-30 16:56:39 +0530

    Kubernetes Completed

--- dags/main.py
+++ dags/main.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 
 default_args = {
     'owner': 'Pulkit Gupta',
-    'start_date': datetime(2022, 3, 21),
+    'start_date': datetime(2022, 3, 30),
     'retries': 1,
     'retry_delay': timedelta(seconds=5)
 }
